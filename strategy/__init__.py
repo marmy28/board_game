56dadfcf343feed30a59b875de1145587fb93074
marmy28 <marmbru28@gmail.com>
2014-10-28 17:22:19 -0500

    Changed Random to RandomPlayer. Fixed mistype.

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,6 +1,6 @@
 from blue_player import BluePlayer
 from green_player import GreenPlayer
-from random_player import Random
+from random_player import RandomPlayer
 from human_player import HumanPlayer
 from burn_player import BurnPlayer
 from play_player import PlayPlayer
