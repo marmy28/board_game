a7ddc7e4e8071eb60599dbd664a176f72c56385d
marmy28 <marmbru28@gmail.com>
2014-10-28 14:31:42 -0500

    Create burn_player.py
    Related to issue #3.

---
+++ strategy/burn_player.py
@@ -0,0 +1,33 @@
+import random
+from person import Person
+
+class BurnPlayer(Person):
+    """This player burns every turn and should end up as the richest player at the end of the game."""
+    difficulty = 0
+    number_in_options = 0
+
+    def __init__(self, name, the_board):
+        super(BurnPlayer, self).__init__(name, the_board)
+
+    def decisionForTurn(self, check_hand=True):
+        player_action = self.chooseAnAction(check_hand)
+        player_card = self.chooseACard()
+        return player_action, player_card
+
+    def chooseAnAction(self, check_hand):
+        """Only selects discarding."""
+        self.use_free_card = False
+        if check_hand:
+            self.use_free_card = True
+            self.checkCardsInHand()
+        self.checkNextWonder()
+        chosenAction = "discardCard"
+        return chosenAction
+
+    def chooseACard(self):
+        """Randomly selects card to discard."""
+        try:
+            choose_card = random.randint(0, (len(self.cards_CAN_play)+len(self.cards_CANNOT_play)-1))
+        except ValueError:
+            choose_card = 0
+        return choose_card
