bf113f031b8ffedd4adfe6043161839794425174
marmy28 <marmbru28@gmail.com>
2014-10-28 14:24:19 -0500

    Create random_player.py
    Related to issue #3.

---
+++ strategy/random_player.py
@@ -0,0 +1,35 @@
+import random
+from person import Person
+
+class RandomPlayer(Person):
+    """This player chooses randomly. Randomly chooses between playing a card or discarding."""
+    difficulty = 1
+    number_in_options = 1
+
+    def __init__(self, name, the_board):
+        super(RandomPlayer, self).__init__(name, the_board)
+
+    def decisionForTurn(self, check_hand=True):
+        player_action = self.chooseAnAction(check_hand)
+        player_card = self.chooseACard()
+        return player_action, player_card
+
+    def chooseAnAction(self, check_hand):
+        """Makes the random choice between playing or discarding."""
+        self.use_free_card = False
+        if check_hand:
+            self.use_free_card = True
+            self.checkCardsInHand()
+        self.checkNextWonder()
+        chosenAction = ["playCard", "discardCard", "playWonder"]
+        random.shuffle(chosenAction)
+        return chosenAction[0]
+
+    def chooseACard(self):
+        """Randomly chooses the card to play."""
+        num_cards = len(self.cards_CAN_play)+len(self.cards_CANNOT_play)
+        try:
+            choose_card = random.randint(0, (num_cards-1))
+        except ValueError:
+            choose_card = 0
+        return choose_card
