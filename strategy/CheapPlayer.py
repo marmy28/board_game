3b40af7bf1a1be7520ab47bef3e34dfd34f20484
marmy28 <marmbru28@gmail.com>
2014-11-03 17:47:38 -0600

    Added in a check for which neighbor is getting the cards next instead of just the 'left' neighbor. Also fixed spacing issues.

--- strategy/CheapPlayer.py
+++ strategy/CheapPlayer.py
@@ -3,7 +3,7 @@ from person import Person
 
 class CheapPlayer(Person):
     """This player will look for the cheapest cards to play."""
-    difficulty = 3
+    difficulty = 2
 
     def __init__(self, name, the_board):
         super(CheapPlayer, self).__init__(name, the_board)
@@ -18,15 +18,15 @@ class CheapPlayer(Person):
         return player_action, player_card
 
     def makeDecision(self):
-		if self.can_afford_wonder and len(self.board.wonders) > 0:
-			if self.board.wonders[0].wonderTotalCost() == 0:
-				return "playWonder", self.burnCard()
+        if self.can_afford_wonder and len(self.board.wonders) > 0:
+            if self.board.wonders[0].wonderTotalCost() == 0:
+                return "playWonder", self.burnCard()
         will_play = []
         for card in enumerate(self.cards_CAN_play):  # card[0] is index, card[1] is card
             if card[1].give_free and card[1].isFree() and card[1].age <= 2:  # this will only work for age 1 and 2
                 will_play.append(card)
-			elif card[1].isFree() and card[1].age == 3:
-				will_play.append(card)
+            elif card[1].isFree() and card[1].age == 3:
+                will_play.append(card)
 
         if len(will_play) == 0:
             return self.minorStrategy()
@@ -62,7 +62,7 @@ class CheapPlayer(Person):
             return "playCard", play_card
 
     def burnCard(self):
-		    color = self.getNeighborColor()
+        color = self.getNeighborColor()
         for index, card in enumerate(self.cards_CANNOT_play):
             if card.color == color:
                 return index + len(self.cards_CAN_play)
@@ -70,13 +70,19 @@ class CheapPlayer(Person):
             return len(self.cards_CAN_play)  # returns the first card in cards_CANNOT_play
         else:
             return 0
-			
-	  def getNeighborColor(self):
-		    max_color = 'green'
-		    max_length = 0
-		    for color in self.neighbor['left'].cards_played.keys():
-			    if color != 'wonder':
-				    if len(self.neighbor['left'].cards_played[color]) > max_length:
-					    max_length = len(self.neighbor['left'].cards_played[color])
-					    max_color = color
-		    return max_color
+
+    def getNeighborColor(self):
+        try:
+            age = self.cards_CAN_play[0].age
+        except IndexError:
+            age = self.cards_CANNOT_play[0].age
+
+        card_direction = {1: 'left', 2: 'right', 3: 'left'}
+        max_color = 'green'
+        max_length = 0
+        for color in self.neighbor[card_direction[age]].cards_played.keys():
+            if color != 'wonder':
+                if len(self.neighbor[card_direction[age]].cards_played[color]) > max_length:
+                    max_length = len(self.neighbor[card_direction[age]].cards_played[color])
+                    max_color = color
+        return max_color
